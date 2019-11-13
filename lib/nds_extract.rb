c0322cbb0bf4e0b51e343d3f6f859c559a28f2f8
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 14:34:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,7 @@ require 'pry'
 
 def directors_totals(nds)
   result = {}
- # binding.pry
+  #binding.pry
   name_index = 0 
   while name_index < nds.length do 
     director = nds[name_index]
@@ -20,6 +20,7 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  binding.pry
  director_index = 0 
  total = 0 
   while director_index < director_data[:movies].length do 
